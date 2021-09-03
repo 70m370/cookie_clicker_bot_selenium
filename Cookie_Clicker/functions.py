4f654d403e7fcb7dca7fc9d3bbb12e712b9bad63
Gabriel <74630076+70m370@users.noreply.github.com>
2021-09-03 17:51:53 -0300

    bug fix

--- Cookie_Clicker/functions.py
+++ Cookie_Clicker/functions.py
@@ -8,12 +8,13 @@ time.sleep(4)   # wait for the page load
 def url_check():
     actual_url = driver.current_url
     while True:
-        if actual_url != ('https://orteil.dashnet.org/cookieclicker/'):
-            driver.get('https://orteil.dashnet.org/cookieclicker/')
-            print("url has changed")
-            return 1
-        else:
+        try:
+            if actual_url != ('https://orteil.dashnet.org/cookieclicker/'):
+                driver.get('https://orteil.dashnet.org/cookieclicker/')
+                print("url has changed")
+        except selenium.common.exceptions.NoSuchElementException:
             return 0
+        return 1
 
 def cookie_click():
     while 1 < 10:
@@ -30,6 +31,8 @@ def cookie_click():
             return 0
         except selenium.common.exceptions.ElementClickInterceptedException:
             return 0
+        except selenium.common.exceptions.NoSuchWindowException:
+            return 0
         return 1
 
 def cookie_upgrade():
@@ -37,30 +40,34 @@ def cookie_upgrade():
         try:
             up_cookie = driver.find_element_by_xpath('/html/body/div/div[2]/div[19]/div[3]/div[5]/div')
             up_cookie.click()
-            print("Time to get better")
+            #print("Time to get better")
         except NoSuchElementException:
             return 0
         except selenium.common.exceptions.ElementNotInteractableException:
             return 0
         except selenium.common.exceptions.StaleElementReferenceException:
             return 0
-         except selenium.common.exceptions.ElementClickInterceptedException:
+        except selenium.common.exceptions.ElementClickInterceptedException:
+            return 0
+        except selenium.common.exceptions.WebDriverException:
             return 0
         time.sleep(0.5)
         return 1
-    
+
 def cookie_workers():
     while 1 < 10:
         try:
             for count in range(18):
                 hire_workers = driver.find_element_by_xpath(f'//*[@id="product{count}"]')
                 hire_workers.click()
-                print("Turn on the heat")
+                #print("Turn on the heat")
         except NoSuchElementException:
             return 0
         except selenium.common.exceptions.ElementNotInteractableException:
             return 0
         except selenium.common.exceptions.ElementClickInterceptedException:
+            return 0
+        except selenium.common.exceptions.StaleElementReferenceException:
             return 0
         time.sleep(0.5)
         return 1
